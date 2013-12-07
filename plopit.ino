f0d57f731cd5e737ae77d1d1ca6f75cb9c131548
acslocum <acslocum@thoughtworks.com>
2013-12-06 23:13:47 -0500

    fixed inputs with internal pullup. just showing inputs, no game happening.

--- plopit.ino
+++ plopit.ino
@@ -6,31 +6,44 @@ int waitCounter = 0;
 int score = 0;
 int currentEvent = 0;
 boolean playing = false;
-const int PLOP_IT = 0;
+const int PLUNGE_IT = 0;
 const int FLUSH_IT = 1;
 const int LIFT_IT = 2;
 
 const int MAX_WAIT_SECONDS = 10;
 const int SLEEP_TIME = 200;
 
-const int PLOP_PIN=2;
+const int PLUNGE_PIN=2;
 const int FLUSH_PIN=4;
 const int LIFT_PIN=6;
 const int NEW_GAME_PIN=8;
 
+const String NEW_GAME_STRING = "plop";
+const String PLUNGE_STRING = "plunge";
+const String LIFT_STRING = "lift";
+const String FLUSH_STRING = "flush";
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello world!");
-  pinMode(PLOP_PIN, INPUT);
+  pinMode(PLUNGE_PIN, INPUT);
   pinMode(FLUSH_PIN, INPUT);
   pinMode(LIFT_PIN, INPUT);
   pinMode(NEW_GAME_PIN, INPUT);
-  newGame(); 
-
+  //newGame();
+  digitalWrite(PLUNGE_PIN,1); 
+  digitalWrite(FLUSH_PIN,1); 
+  digitalWrite(LIFT_PIN,1); 
+  digitalWrite(NEW_GAME_PIN,1); 
+  delay(200);
 }
 
 
 void loop() {
+  readInputs();
+}
+
+void playGame() {
   if(newGamePressed()) {
     newGame(); 
   }
@@ -45,6 +58,26 @@ void loop() {
     checkForIncorrectResponse();
   }
   delay(SLEEP_TIME);
+  
+}
+
+void readInputs() {
+ //expectPlungeIt();
+ //expectLiftIt();
+ //expectFlushIt();
+ //newGamePressed();
+ 
+ if(expectPlungeIt()) {
+  Serial.println(PLUNGE_STRING); 
+ } else if(!expectLiftIt()) {
+   Serial.println(LIFT_STRING);
+ } else if(expectFlushIt()) {
+   Serial.println(FLUSH_STRING);
+ } else if(newGamePressed()) {
+   Serial.println(NEW_GAME_STRING);
+ }
+ 
+ delay(10*100); 
 }
 
 
@@ -81,10 +114,6 @@ void gameOverActions() {
   delay(4000);
 }
 
-boolean newGamePressed() {
-  digitalRead(NEW_GAME_PIN)==HIGH;
-}
-
 void newGame() {
   waitCounter = 0;
   score = 0;
@@ -98,8 +127,8 @@ boolean gameOn() {
 }
 
 boolean correctResponse() {
-  if(currentEvent==PLOP_IT) {
-    return expectPlopIt();
+  if(currentEvent==PLUNGE_IT) {
+    return expectPlungeIt();
   } else if(currentEvent == FLUSH_IT) {
     return expectFlushIt(); 
   } else {
@@ -108,26 +137,37 @@ boolean correctResponse() {
 }
 
 boolean incorrectResponse() {
-  if(currentEvent==PLOP_IT) {
+  if(currentEvent==PLUNGE_IT) {
     return expectFlushIt() || expectLiftIt();
   } else if(currentEvent==FLUSH_IT) {
-    return expectPlopIt() || expectLiftIt();
+    return expectPlungeIt() || expectLiftIt();
   } else {
-    return expectPlopIt() || expectFlushIt(); 
+    return expectPlungeIt() || expectFlushIt(); 
   }
 }
 
-boolean expectPlopIt() {
-  return digitalRead(PLOP_PIN) == HIGH;
-  //return true;
+boolean expectPlungeIt() {
+  //Serial.print("Plunge: ");
+  //Serial.println(digitalRead(PLUNGE_PIN));
+  return digitalRead(PLUNGE_PIN) == LOW;
 }
 
 boolean expectFlushIt() {
-  return digitalRead(FLUSH_PIN) == HIGH;
+  //Serial.print("Flush: ");
+  //Serial.println(digitalRead(FLUSH_PIN));
+  return digitalRead(FLUSH_PIN) == LOW;
 }
 
 boolean expectLiftIt() {
+  //Serial.print("Lift: ");
+  //Serial.println(digitalRead(LIFT_PIN));
   return digitalRead(LIFT_PIN) == HIGH;
+}
+
+boolean newGamePressed() {
+  //Serial.print("NewGame: ");
+  //Serial.println(digitalRead(NEW_GAME_PIN));
+  return digitalRead(NEW_GAME_PIN)==LOW;
 }
 
 boolean waitTimeExceeded(int count) {
@@ -149,7 +189,7 @@ int generateEvent() {
 
 void returnEvent(int event) {
   //Serial.println(event);
-  if(event == PLOP_IT) {
+  if(event == PLUNGE_IT) {
     sendMessage("PlopIt");
   } else if(event == FLUSH_IT) {
     sendMessage("FlushIt");
